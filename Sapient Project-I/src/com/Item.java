bf96885bc3cc505336ae4575494f9e6bc3792bf4
Amrendra Singh <amrendra.13oct@gmail.com>
2015-09-04 10:22:10 +0530

    description created and getter setter initialized

--- Sapient Project-I/src/com/Item.java
+++ Sapient Project-I/src/com/Item.java
@@ -5,5 +5,9 @@ public class Item {
 	private double shippingWeight;
 	public double getShippingWeight() {return shippingWeight;}
 	public void setShippingWeight(double shippingWeight) {this.shippingWeight = shippingWeight;}
+	
+	private String description;
+	public String getDescription() {return description;}
+	public void setDescription(String description) {this.description = description;}
 
 }
