35e2938d0c2ca2990fb413d3cbf4c49f274d3b50
Amrendra Singh <amrendra.13oct@gmail.com>
2015-09-04 10:13:18 +0530

    cashTendered object created and getter-setter initialized

--- Sapient Project-I/src/com/Cash.java
+++ Sapient Project-I/src/com/Cash.java
@@ -1,5 +1,14 @@
 package com;
 
 public class Cash extends Payment{
-
+	
+	private double cashTendered; // declaration of variable cashTender of type double
+	//getter method for cashTended
+	public double getCashTendered() {
+		return cashTendered;
+	}
+	//setter method for cashTender
+		public void setCashTendered(double cashTendered) {
+			this.cashTendered = cashTendered;
+		}
 }
