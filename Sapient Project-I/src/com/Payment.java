fc3ce14e7d4fc700945b0fce76ce24ba1594f47d
Amrendra Singh <amrendra.13oct@gmail.com>
2015-09-04 10:28:20 +0530

    order created and getter setter initialized

--- Sapient Project-I/src/com/Payment.java
+++ Sapient Project-I/src/com/Payment.java
@@ -5,5 +5,9 @@ public class Payment {
 	private double amount;
 	public double getAmount() {return amount;}
 	public void setAmount(double amount) {this.amount = amount;}
+	
+	private Order newOrder;
+	public Order getNewOrder() {return newOrder;}
+	public void setNewOrder(Order newOrder) {this.newOrder = new Order();}
 
 }
