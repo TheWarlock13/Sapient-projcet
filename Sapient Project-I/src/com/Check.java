57bd130b20903ea1f1a163ebf61b3cbe57ce37eb
Amrendra Singh <amrendra.13oct@gmail.com>
2015-09-04 10:17:20 +0530

    name created and getter-setter initialized

--- Sapient Project-I/src/com/Check.java
+++ Sapient Project-I/src/com/Check.java
@@ -2,4 +2,8 @@ package com;
 
 public class Check extends Payment implements Authorization{
 
+	private String name;
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
+
 }
