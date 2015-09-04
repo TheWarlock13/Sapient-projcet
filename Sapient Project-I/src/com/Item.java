2a4e6fa72e34c1d878601159d25497e7dea52822
Amrendra Singh <amrendra.13oct@gmail.com>
2015-09-04 10:21:08 +0530

    shippingWeight created and getter setter initialized

--- Sapient Project-I/src/com/Item.java
+++ Sapient Project-I/src/com/Item.java
@@ -1,5 +1,9 @@
 package com;
 
 public class Item {
+	
+	private double shippingWeight;
+	public double getShippingWeight() {return shippingWeight;}
+	public void setShippingWeight(double shippingWeight) {this.shippingWeight = shippingWeight;}
 
 }
