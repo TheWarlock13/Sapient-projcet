09d267063b5efe74338f40e2b9a3cb52403d8bfa
Amrendra Singh <amrendra.13oct@gmail.com>
2015-09-04 10:18:46 +0530

    customerBankID created and getter setter initialized

--- Sapient Project-I/src/com/Check.java
+++ Sapient Project-I/src/com/Check.java
@@ -5,5 +5,9 @@ public class Check extends Payment implements Authorization{
 	private String name;
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name;}
+	
+	private long customerBankId;
+	public long getCustomerBankId() {return customerBankId;}
+	public void setCustomerBankId(long customerBankId) {this.customerBankId = customerBankId;}
 
 }
